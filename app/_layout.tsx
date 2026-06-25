dbd86d5a16dda1b9d7ce7e65ed224a0972964f4e
Alexander Gutierrez Sajonero <agutierr94@cuc.edu.co>
2026-06-24 19:39:12 -0500

    chore: upload app/_layout.tsx

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import "../global.css";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+
+export default function RootLayout() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "#000000" }}>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: "#000000" },
+          animation: "fade",
+        }}
+      />
+    </View>
+  );
+}
